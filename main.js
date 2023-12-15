dc0d6fb391b4f2f18febccd50d00f514927b11aa
Rubik <pnazarshoev33@gmail.com>
2023-12-15 13:47:14 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,59 @@
+//Task1
+// function sumTwoNumber(a,b){
+//     console.log(a+b);
+// }
+
+// sumTwoNumber(2,3);
+// sumTwoNumber(4,4);
+// sumTwoNumber(-2,4);
+
+
+//Task2
+
+// function checkNumber (a){
+//     if(a > 0){
+//         console.log("Positive");
+//     }
+//     else if (a < 0){
+//         console.log("Negative");
+//     }
+//     else {
+//         console.log("Zero");
+//     }
+// }
+
+// checkNumber(5);
+// checkNumber(-8);
+// checkNumber(0);
+
+
+
+// Task 3
+
+// function generateMultiplicationTable(a){
+//     for(let i = 1; i <= 10;i++){
+//         console.log(a + "*" + i + "=" + a * i);
+//     }
+// }
+
+// generateMultiplicationTable(5);
+
+//Task4
+
+// function endNine(a){
+//     if(a % 10 == 9){
+//         console.log("Yes");
+//     }
+//     else {
+//         console.log("No");
+//         }
+// }
+
+// endNine(123);
+// endNine(129);
+
+//Task5
+
+function century(a){
+    
+}
